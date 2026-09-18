1f7c75e3cf98e16b5473ccfd1630e389321001dc
Parinay Joshi <parinayprakash1981@gmail.com>
2026-09-18 19:28:43 +0530

    Add files via upload

---
+++ day3_q6.c
@@ -0,0 +1,35 @@
+/* 
+Question 6 (User Inputs, Operations & Output) - Day 3
+Write a program to swap two numbers using a third variable.
+
+Sample Test Cases:
+Input 1:
+3 5
+Output 1:
+After swap: 5 3
+
+Input 2:
+-1 1
+Output 2:
+After swap: 1 -1
+*/
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, temp;
+
+    printf("Enter 1st number: ");
+    scanf("%d", &num1);
+
+    printf("Enter 2nd number: ");
+    scanf("%d", &num2);
+
+    temp = num1;   
+    num1 = num2;
+    num2 = temp;
+
+    printf("After swap: %d %d\n", num1, num2);
+
+    return 0;
+}
